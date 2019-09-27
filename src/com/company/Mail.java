cf9123e607dc8b8998b50464c1da6ce9def4c293
vladshevchuk <vlad.shevchuk1@icloud.com>
2019-09-27 13:00:11 +0300

    Test

--- src/com/company/Mail.java
+++ src/com/company/Mail.java
@@ -2,6 +2,9 @@ package com.company;
 
 public class Mail {
     public static void main(String[] args) {
-        System.out.println("vlad  krutyi  chuvak");
+        System.out.println("\t\tJ\t\t\t\t\t\t\tA");
+        System.out.println("J\t\tJ");
+        System.out.println("J\t\tJ");
+        System.out.println("J\t\tJj");
     }
 }
