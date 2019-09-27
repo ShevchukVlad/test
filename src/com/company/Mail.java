0158dc0028ad573961dd0112def1c676e927b201
vladshevchuk <vlad.shevchuk1@icloud.com>
2019-09-27 15:27:23 +0300

    text

--- src/com/company/Mail.java
+++ src/com/company/Mail.java
@@ -2,14 +2,10 @@ package com.company;
 
 public class Mail {
     public static void main(String[] args) {
-        System.out.println("\t\tJ\t\t\t\t\t\t\tA");
-        System.out.println("J\t\tJ");
-        System.out.println("J\t\tJ");
-        System.out.println("J\t\tJ");
-        System.out.println("J\t\tJ");
-        System.out.println("J\t\tJ");
-        System.out.println("J\t\tJ");
-        System.out.println("J\t\tJj");
-        System.out.println("J\t\tJj");
+        System.out.println("\t\t\fJ\t\t\tA\t\t\f\f\fV\t\t\tV\t\t\tA");
+        System.out.println("\t\t\fJ\t\t\f\f\f\f\fA\f\fA\t\t\f\f\f\fV\t\t\f\f\f\f\fV\t\t\f\f\f\f\fA\f\fA");
+        System.out.println("\t\t\fJ\t\t\f\f\fA\t\f\f\f\fA\t\t\f\f\f\f\fV\t\f\f\fV\t\t\t\f\f\fA\t\f\f\f\fA");
+        System.out.println("\fJ\t\t\fJ\t\t\fAAAAAAAA\t\tV\t\fV\t\t\t\fAAAAAAAA");
+        System.out.println("\fJ\fJ\fJ\fJ\fJ\fJ\t\tA\t\tA\t\t\f\f\f\fV\t\t\t\tA\t\tA");
     }
 }
