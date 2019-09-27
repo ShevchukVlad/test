a62d4c70e9bd6692876976a7f3dda62c3ba4cb37
vladshevchuk <vlad.shevchuk1@icloud.com>
2019-09-27 12:17:29 +0300

    new

--- src/com/company/Mail.java
+++ src/com/company/Mail.java
@@ -2,6 +2,6 @@ package com.company;
 
 public class Mail {
     public static void main(String[] args) {
-        System.out.println("helloworl");
+        System.out.println("vlad  krutyi  chuvak");
     }
 }
